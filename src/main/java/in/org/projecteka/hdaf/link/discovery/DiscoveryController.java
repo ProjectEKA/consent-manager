ba3087d41875e3c08ccce703af23f861536ac111
chethandeshpande <chethandec22@gmail.com>
2019-12-30 15:19:44 +0530

    [EKA-7] Moved /providers mapping to method level

--- src/main/java/in/org/projecteka/hdaf/link/discovery/DiscoveryController.java
+++ src/main/java/in/org/projecteka/hdaf/link/discovery/DiscoveryController.java
@@ -2,19 +2,17 @@ package in.org.projecteka.hdaf.link.discovery;
 
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/providers")
 @AllArgsConstructor
 public class DiscoveryController {
 
     private Discovery discovery;
 
-    @GetMapping
+    @GetMapping("/providers")
     public Flux<ProviderRepresentation> getProvidersByName(@RequestParam String name) {
         return discovery.providersFrom(name);
     }

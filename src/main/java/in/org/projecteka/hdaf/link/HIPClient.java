51b0ebaf662efe6e225e55de0029fd13b69778a4
Apoorva-GA <apoorvaga29@gmail.com>
2020-01-08 10:48:36 +0530

    [EKA-12] Removing fully qualified name when not needed

--- src/main/java/in/org/projecteka/hdaf/link/HIPClient.java
+++ src/main/java/in/org/projecteka/hdaf/link/HIPClient.java
@@ -1,9 +1,12 @@
 package in.org.projecteka.hdaf.link;
 
+import in.org.projecteka.hdaf.link.link.model.PatientLinkReferenceRequest;
 import in.org.projecteka.hdaf.link.link.model.PatientLinkReferenceResponse;
 import in.org.projecteka.hdaf.link.link.model.PatientLinkRequest;
 import in.org.projecteka.hdaf.link.link.model.PatientLinkResponse;
+import in.org.projecteka.hdaf.link.link.model.ErrorRepresentation;
 import in.org.projecteka.hdaf.link.link.model.hip.Patient;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
@@ -17,7 +20,7 @@ public class HIPClient {
         this.webClientBuilder = webClientBuilder;
     }
 
-    public Mono<PatientLinkReferenceResponse> linkPatientCareContext(String patientId, in.org.projecteka.hdaf.link.link.model.PatientLinkReferenceRequest patientLinkReferenceRequest, String url) {
+    public Mono<PatientLinkReferenceResponse> linkPatientCareContext(String patientId, PatientLinkReferenceRequest patientLinkReferenceRequest, String url) {
         Patient patientInHIP = toHIPPatient(patientId, patientLinkReferenceRequest);
 
         in.org.projecteka.hdaf.link.link.model.hip.PatientLinkReferenceRequest patientLinkReferenceRequestHIP = new in.org.projecteka.hdaf.link.link.model.hip.PatientLinkReferenceRequest(
@@ -26,7 +29,7 @@ public class HIPClient {
         return webClientBuilder.build()
                 .post()
                 .uri(String.format("%s/patients/link", url))
-                .body(Mono.just(patientLinkReferenceRequestHIP), in.org.projecteka.hdaf.link.link.model.PatientLinkReferenceRequest.class)
+                .body(Mono.just(patientLinkReferenceRequestHIP), PatientLinkReferenceRequest.class)
                 .retrieve()
                 .bodyToMono(PatientLinkReferenceResponse.class);
     }

c7bf50491c95e715326198d6b6481cc92b65e57a
Angshuman Sarkar <angshuonline@gmail.com>
2020-01-21 12:05:20 +0530

    EKA-19 | Fixing codacy and sonar inputs

--- src/test/java/in/projecteka/consentmanager/consent/service/ConsentManagerTest.java
+++ src/test/java/in/projecteka/consentmanager/consent/service/ConsentManagerTest.java
@@ -1,5 +1,6 @@
 package in.projecteka.consentmanager.consent.service;
 
+import in.projecteka.consentmanager.clients.ClientError;
 import in.projecteka.consentmanager.clients.ClientRegistryClient;
 import in.projecteka.consentmanager.clients.UserServiceClient;
 import in.projecteka.consentmanager.clients.model.Provider;
@@ -33,12 +34,12 @@ class ConsentManagerTest {
     private UserServiceClient userClient;
 
     @BeforeEach
-    void setUp() {
+    private void setUp() {
         initMocks(this);
     }
 
     @Test
-    void askForConsent() {
+    public void askForConsent() {
         HIPReference hip1 = HIPReference.builder().id("hip1").build();
         HIUReference hiu1 = HIUReference.builder().id("hiu1").build();
         PatientReference patient = PatientReference.builder().id("chethan@ncg").build();
@@ -52,6 +53,24 @@ class ConsentManagerTest {
 
         String requestingHIUId = "hiu1";
         StepVerifier.create(consentManager.askForConsent(requestingHIUId, consentDetail)).expectNextMatches(r -> r != null).verifyComplete();
+    }
+
+    @Test
+    public void askForConsentWithoutValidHIU() {
+        HIPReference hip1 = HIPReference.builder().id("hip1").build();
+        HIUReference hiu1 = HIUReference.builder().id("hiu1").build();
+        PatientReference patient = PatientReference.builder().id("chethan@ncg").build();
+        ConsentDetail consentDetail = ConsentDetail.builder().hip(hip1).hiu(hiu1).patient(patient).build();
+
+        ConsentManager consentManager = new ConsentManager(repository, providerClient, userClient);
+        when(repository.insert(any(), any())).thenReturn(Mono.empty());
+        when (providerClient.providerWith(eq("hip1"))).thenReturn(Mono.just(new Provider()));
+        when (providerClient.providerWith(eq("hiu1"))).thenReturn(Mono.error(ClientError.providerNotFound()));
+        when (userClient.userOf(eq("chethan@ncg"))).thenReturn(Mono.just(new User()));
+
+        String requestingHIUId = "hiu1";
+        StepVerifier.create(consentManager.askForConsent(requestingHIUId, consentDetail))
+                .expectErrorMatches(e -> (e instanceof ClientError) && ((ClientError) e).getHttpStatus().is4xxClientError());
     }
 
 

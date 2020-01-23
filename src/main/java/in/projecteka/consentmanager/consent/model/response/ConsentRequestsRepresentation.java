92f10afe92f2394e029a93e48a4f4485fafe8050
Angshuman Sarkar <angshuonline@gmail.com>
2020-01-23 12:31:58 +0530

    EKA-19 | fixing codacy inputs

--- src/main/java/in/projecteka/consentmanager/consent/model/response/ConsentRequestsRepresentation.java
+++ src/main/java/in/projecteka/consentmanager/consent/model/response/ConsentRequestsRepresentation.java
@@ -1,7 +1,11 @@
 package in.projecteka.consentmanager.consent.model.response;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 

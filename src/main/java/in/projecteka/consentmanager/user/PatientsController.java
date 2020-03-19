63042ea2767bd387aa70ead87ccea61f73ac3dc6
chethandeshpande <chethandec22@gmail.com>
2020-03-19 18:19:39 +0530

    Change swagger for verify pin

--- src/main/java/in/projecteka/consentmanager/user/PatientsController.java
+++ src/main/java/in/projecteka/consentmanager/user/PatientsController.java
@@ -7,10 +7,12 @@ import in.projecteka.consentmanager.user.model.Token;
 import in.projecteka.consentmanager.user.model.ValidatePinRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -21,6 +23,7 @@ public class PatientsController {
     private ProfileService profileService;
     private TransactionPinService transactionPinService;
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     @PostMapping("/pin")
     public Mono<Void> pin(@RequestBody CreatePinRequest createPinRequest) {
         return ReactiveSecurityContextHolder.getContext()

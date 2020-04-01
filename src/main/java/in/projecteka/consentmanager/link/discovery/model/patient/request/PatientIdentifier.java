16270b8c7d6313d45c1aa70b777194cefa66de3d
Angshuman Sarkar <angshuonline@gmail.com>
2020-04-01 19:04:26 +0530

    EKA-151 | Extracting out constants for validation messages

--- src/main/java/in/projecteka/consentmanager/link/discovery/model/patient/request/PatientIdentifier.java
+++ src/main/java/in/projecteka/consentmanager/link/discovery/model/patient/request/PatientIdentifier.java
@@ -6,12 +6,15 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 
+import static in.projecteka.consentmanager.link.discovery.ValidationMessages.PATIENT_IDENTIFIER_TYPE_NOT_SPECIFIED;
+import static in.projecteka.consentmanager.link.discovery.ValidationMessages.PATIENT_IDENTIFIER_VALUE_NOT_SPECIFIED;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PatientIdentifier {
-    @NotBlank(message = "Identifier Type must be specified")
+    @NotBlank(message = PATIENT_IDENTIFIER_TYPE_NOT_SPECIFIED)
     private PatientIdentifierType type;
-    @NotBlank(message = "Identifier Value must be specified")
+    @NotBlank(message = PATIENT_IDENTIFIER_VALUE_NOT_SPECIFIED)
     private String value;
 }

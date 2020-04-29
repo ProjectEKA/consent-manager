fb7b418222c4351874a7d072688a1962746d9f34
rishabGhoshtw <rishabgh@thoughtworks.com>
2020-04-29 17:55:21 +0530

    (#41) add mocks in keypair config test file

--- src/test/java/in/projecteka/consentmanager/common/KeyPairConfigTest.java
+++ src/test/java/in/projecteka/consentmanager/common/KeyPairConfigTest.java
@@ -1,12 +1,20 @@
 package in.projecteka.consentmanager.common;
 
 import com.nimbusds.jose.jwk.JWKSet;
+import in.projecteka.consentmanager.DestinationsConfig;
+import in.projecteka.consentmanager.consent.ConsentRequestNotificationListener;
+import in.projecteka.consentmanager.consent.HipConsentNotificationListener;
+import in.projecteka.consentmanager.consent.HiuConsentNotificationListener;
+import in.projecteka.consentmanager.dataflow.DataFlowBroadcastListener;
+import in.projecteka.consentmanager.user.SignUpService;
+import in.projecteka.consentmanager.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.security.KeyPair;
 
@@ -25,6 +33,28 @@ class KeyPairConfigTest {
     @MockBean(name = "identityServiceJWKSet")
     private JWKSet identityServiceJWKSet;
 
+    @MockBean
+    private UserService userService;
+
+    @MockBean
+    private DestinationsConfig destinationsConfig;
+
+    @MockBean
+    private HiuConsentNotificationListener hiuConsentNotificationListener;
+
+    @MockBean
+    private HipConsentNotificationListener hipConsentNotificationListener;
+
+    @MockBean
+    private DataFlowBroadcastListener dataFlowBroadcastListener;
+
+    @SuppressWarnings("unused")
+    @MockBean
+    private ConsentRequestNotificationListener consentRequestNotificationListener;
+
+    @MockBean
+    private SignUpService signupService;
+    
     @Test
     void shouldCreateSignArtefactKeyPair() {
         KeyPair signArtefactKeyPair = keyPairConfig.createSignArtefactKeyPair();

1b6f7aeabcfa9438c28f46deb05015e115c3ce70
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-05-06 10:55:08 +0530

    [CM-BE-6] | Leela Prasanth/Zafar | Handles the things in SessionService to either Adding or the updating the user to LockedUSerRepo

--- src/main/java/in/projecteka/consentmanager/user/SessionService.java
+++ src/main/java/in/projecteka/consentmanager/user/SessionService.java
@@ -3,6 +3,8 @@ package in.projecteka.consentmanager.user;
 import in.projecteka.consentmanager.clients.ClientError;
 import in.projecteka.consentmanager.clients.model.Session;
 import in.projecteka.consentmanager.common.cache.CacheAdapter;
+import in.projecteka.consentmanager.user.exception.InvalidUserNameException;
+import in.projecteka.consentmanager.user.model.LockedUser;
 import in.projecteka.consentmanager.user.model.LogoutRequest;
 import in.projecteka.consentmanager.user.model.SessionRequest;
 import lombok.AllArgsConstructor;
@@ -10,6 +12,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 import reactor.core.publisher.Mono;
+
+import java.util.Date;
 
 import static in.projecteka.consentmanager.common.Constants.BLACKLIST;
 import static in.projecteka.consentmanager.common.Constants.BLACKLIST_FORMAT;
@@ -20,13 +24,33 @@ public class SessionService {
     private final TokenService tokenService;
     private final CacheAdapter<String, String> blacklistedTokens;
     private final Logger logger = LoggerFactory.getLogger(SessionService.class);
+    private final LockedUsersRepository lockedUsersRepository;
 
     public Mono<Session> forNew(SessionRequest request) {
         if (StringUtils.isEmpty(request.getUsername()) || StringUtils.isEmpty(request.getPassword()))
             return Mono.error(ClientError.unAuthorizedRequest());
         return tokenService.tokenForUser(request.getUsername(), request.getPassword())
                 .doOnError(error -> logger.error(error.getMessage(), error))
-                .onErrorResume(error -> Mono.error(ClientError.unAuthorizedRequest()));
+                .onErrorResume(InvalidUserNameException.class, error -> Mono.error(ClientError.unAuthorizedRequest1()))
+                .onErrorResume(error -> lockedUsersRepository.getLockedUserFor(request.getUsername())
+                        .switchIfEmpty(
+                                lockedUsersRepository.insert(
+                                        new LockedUser(1, request.getUsername(), false, null))
+                                        .then(Mono.error(ClientError.unAuthorizedRequest())))
+                        .flatMap(optionalLockedUser -> {
+                            var isLocked = false;
+                            var blockedTime = "";
+                            var clientError = ClientError.unAuthorizedRequest();
+                            if (optionalLockedUser.getInvalidAttempts() >= 5) {
+                                isLocked = true;
+                                blockedTime = new Date().toString();
+                                clientError = ClientError.userBlocked();
+                            }
+                            return lockedUsersRepository
+                                    .updateUser(isLocked, blockedTime,
+                                            optionalLockedUser.getPatientId(), optionalLockedUser.getInvalidAttempts() + 1)
+                                    .then(Mono.error(clientError));
+                        }));
     }
 
     public Mono<Void> logout(String accessToken, LogoutRequest logoutRequest) {

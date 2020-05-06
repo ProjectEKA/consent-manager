66d9f0b9644fb9ea6f5c02a4bd7c9b10fb6f2629
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-05-06 10:50:39 +0530

    [CM-BE-6] | Leela Prasanth/Zafar | Adds InvalidPasswordException and InvalidUserNameException for the proper error handling

---
+++ src/main/java/in/projecteka/consentmanager/user/exception/InvalidUserNameException.java
@@ -0,0 +1,8 @@
+package in.projecteka.consentmanager.user.exception;
+
+public class InvalidUserNameException extends RuntimeException {
+    public InvalidUserNameException(String message) {
+        super(message);
+    }
+}
+

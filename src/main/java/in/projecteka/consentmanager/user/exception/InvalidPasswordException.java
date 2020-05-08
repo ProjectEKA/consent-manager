91559ea1b4fe180a9939448c9b159fdb6f63185a
leelanakka <44019459+leelanakka@users.noreply.github.com>
2020-05-08 11:35:15 +0530

    [CM-BE-6] | Leela Prasanth | Removes unnecessary Constructor

--- src/main/java/in/projecteka/consentmanager/user/exception/InvalidPasswordException.java
+++ src/main/java/in/projecteka/consentmanager/user/exception/InvalidPasswordException.java
@@ -1,8 +1,6 @@
 package in.projecteka.consentmanager.user.exception;
 
 public class InvalidPasswordException extends RuntimeException {
-    public InvalidPasswordException() {
-        super();
-    }
+
 }
 
--- src/main/java/in/projecteka/consentmanager/user/exception/InvalidUserNameException.java
+++ src/main/java/in/projecteka/consentmanager/user/exception/InvalidUserNameException.java
@@ -1,8 +1,5 @@
 package in.projecteka.consentmanager.user.exception;
 
 public class InvalidUserNameException extends RuntimeException {
-    public InvalidUserNameException() {
-        super();
-    }
 }
 

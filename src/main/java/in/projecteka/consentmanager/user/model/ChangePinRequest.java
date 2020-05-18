d527a65ab14bb11006818e87f973c3224274c24b
gayatrijagtap <gdjagtap1999@gmail.com>
2020-05-18 11:33:04 +0530

    [CM_APP_36] Gayatri | remove unneccessary imports

--- src/main/java/in/projecteka/consentmanager/user/model/ChangePinRequest.java
+++ src/main/java/in/projecteka/consentmanager/user/model/ChangePinRequest.java
@@ -4,8 +4,6 @@ package in.projecteka.consentmanager.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-import java.util.UUID;
-
 @AllArgsConstructor
 @Getter
 public class ChangePinRequest {

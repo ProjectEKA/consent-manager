03f72276b4669e60df9b4d7c8d5dc498ee894df6
Dheeraj Pundir <1236dheeraj.ds@gmail.com>
2020-05-26 15:08:34 +0530

    CM_APP_48 | Dheeraj | Reset invalid attempt if an attmept is made after cool of period

--- src/main/java/in/projecteka/consentmanager/user/LockedUserService.java
+++ src/main/java/in/projecteka/consentmanager/user/LockedUserService.java
@@ -2,24 +2,18 @@ package in.projecteka.consentmanager.user;
 
 import in.projecteka.consentmanager.clients.ClientError;
 import in.projecteka.consentmanager.user.model.LockedUser;
-import in.projecteka.consentmanager.user.model.OtpAttempt;
 import lombok.AllArgsConstructor;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Mono;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
-import java.util.Date;
 
 @AllArgsConstructor
 public class LockedUserService {
     private final LockedUsersRepository lockedUsersRepository;
     private final LockedServiceProperties lockedServiceProperties;
 
-    public Mono<Void> validateLogin(String cmId){
+    public Mono<Void> validateLogin(String cmId) {
         return lockedUsersRepository.getLockedUserFor(cmId)
                 .filter(lockedUser -> lockedUser.getInvalidAttempts() >= lockedServiceProperties.getMaximumInvalidAttempts())
                 .flatMap(lockedUser -> {
@@ -28,12 +22,15 @@ public class LockedUserService {
                 });
     }
 
-    public Mono<Void> removeLockedUser(String cmId){
+    public Mono<Void> removeLockedUser(String cmId) {
         return lockedUsersRepository.deleteUser(cmId);
     }
 
-    public Mono<Void> createOrUpdateLockedUser(String cmId){
-        return lockedUsersRepository.upsert(cmId);
+    public Mono<Void> createOrUpdateLockedUser(String cmId) {
+        return lockedUsersRepository.getLockedUserFor(cmId)
+                .flatMap(lockedUser ->
+                        isWithinTimeLimit(lockedUser, lockedServiceProperties.getCoolOfPeriod()) ? Mono.empty() : removeLockedUser(cmId))
+                .then(lockedUsersRepository.upsert(cmId));
     }
 
     private boolean isWithinTimeLimit(LockedUser user, int timeLimitInMin) {

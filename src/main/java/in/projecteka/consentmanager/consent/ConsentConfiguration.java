762d11ce6edfaa34f77c6cf8711c47aeb2723362
saisree <saisree@thoughtworks.com>
2020-06-04 13:17:03 +0530

    G-CM-35 | Refactor CM to POST consent-artefacts to HIP

--- src/main/java/in/projecteka/consentmanager/consent/ConsentConfiguration.java
+++ src/main/java/in/projecteka/consentmanager/consent/ConsentConfiguration.java
@@ -94,7 +94,10 @@ public class ConsentConfiguration {
             ConsentRequestRepository repository,
             ConsentArtefactRepository consentArtefactRepository,
             ConsentNotificationPublisher consentNotificationPublisher) {
-        return new ConsentScheduler(repository, consentArtefactRepository, consentNotificationPublisher);
+        return new ConsentScheduler(
+                repository,
+                consentArtefactRepository,
+                consentNotificationPublisher);
     }
 
     @Bean

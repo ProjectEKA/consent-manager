c8f8ab5e1a499e1a6819caa8ce957d7cea24acc7
Zafar <momin.zafaraabid@thoughtworks.com>
2020-06-09 11:47:52 +0530

    EKA-G-CM-37   added test case for notifyHiu api

--- src/test/java/in/projecteka/consentmanager/clients/ConsentArtefactNotifierTest.java
+++ src/test/java/in/projecteka/consentmanager/clients/ConsentArtefactNotifierTest.java
@@ -2,6 +2,9 @@ package in.projecteka.consentmanager.clients;
 
 import in.projecteka.consentmanager.clients.properties.GatewayServiceProperties;
 import in.projecteka.consentmanager.consent.model.request.HIPNotificationRequest;
+import in.projecteka.consentmanager.consent.model.request.ConsentNotifier;
+import in.projecteka.consentmanager.consent.model.request.HIUNotificationRequest;
+import in.projecteka.consentmanager.dataflow.model.hip.DataFlowRequest;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -21,11 +24,18 @@ import reactor.test.StepVerifier;
 import static in.projecteka.consentmanager.clients.HeaderConstants.HDR_HIP_ID;
 import static in.projecteka.consentmanager.clients.TestBuilders.string;
 import static org.mockito.Mockito.when;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import static in.projecteka.consentmanager.dataflow.TestBuilders.dataFlowRequestBuilder;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 public class ConsentArtefactNotifierTest {
     @Captor
     private ArgumentCaptor<ClientRequest> captor;
     private ConsentArtefactNotifier consentArtefactNotifier;
+
     @Mock
     private ExchangeFunction exchangeFunction;
 
@@ -86,5 +96,68 @@ public class ConsentArtefactNotifierTest {
                 .expectErrorMatches(throwable -> throwable instanceof ClientError &&
                         ((ClientError) throwable).getHttpStatus().is5xxServerError())
                 .verify();
+    }
+
+    @Test
+    void sendConsentArtifactToHIU() {
+            var token = string();
+            when(exchangeFunction.exchange(captor.capture())).thenReturn(Mono.just(ClientResponse.create(HttpStatus.OK)
+                    .header("Content-Type", "application/json")
+                    .build()));
+            WebClient.Builder webClientBuilder = WebClient.builder().exchangeFunction(exchangeFunction);
+            DataFlowRequest dataFlowRequest = dataFlowRequestBuilder().build();
+            GatewayServiceProperties serviceProperties = new GatewayServiceProperties("http://example.com", 2000);
+            ConsentArtefactNotifier dataRequestNotifier = new ConsentArtefactNotifier(webClientBuilder, () -> Mono.just(token),serviceProperties);
+
+        HIUNotificationRequest request = new HIUNotificationRequest(LocalDateTime.now(), UUID.randomUUID(),new ConsentNotifier());
+            StepVerifier.create(dataRequestNotifier.sendConsentArtifactToHIU(request, "1000005"))
+                    .verifyComplete();
+
+            assertThat(captor.getValue().url().toString()).isEqualTo("http://example.com/consents/hiu/notify");
+            assertThat(captor.getValue().headers().getFirst(AUTHORIZATION)).isEqualTo(token);
+
+    }
+
+    @Test
+    void shouldThrowExceptionWhenUnAuthorizedTokenPass() {
+        var token = string();
+        when(exchangeFunction.exchange(captor.capture())).thenReturn(Mono.just(ClientResponse.create(HttpStatus.UNAUTHORIZED)
+                .header("Content-Type", "application/json")
+                .build()));
+        WebClient.Builder webClientBuilder = WebClient.builder().exchangeFunction(exchangeFunction);
+        DataFlowRequest dataFlowRequest = dataFlowRequestBuilder().build();
+        GatewayServiceProperties serviceProperties = new GatewayServiceProperties("http://example.com", 2000);
+        ConsentArtefactNotifier dataRequestNotifier = new ConsentArtefactNotifier(webClientBuilder, () -> Mono.just(token),serviceProperties);
+
+        HIUNotificationRequest request = new HIUNotificationRequest(LocalDateTime.now(), UUID.randomUUID(),new ConsentNotifier());
+        StepVerifier.create(dataRequestNotifier.sendConsentArtifactToHIU(request, "1000005"))
+                .expectErrorMatches(throwable -> throwable instanceof ClientError &&
+                        ((ClientError) throwable).getHttpStatus().is4xxClientError())
+                .verify();
+        assertThat(captor.getValue().url().toString()).isEqualTo("http://example.com/consents/hiu/notify");
+        assertThat(captor.getValue().headers().getFirst(AUTHORIZATION)).isEqualTo(token);
+
+    }
+
+    @Test
+    void ShouldThrowExceptionWhenInternalServerGetError() {
+        var token = string();
+        when(exchangeFunction.exchange(captor.capture())).thenReturn(Mono.just(ClientResponse
+                .create(HttpStatus.INTERNAL_SERVER_ERROR)
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .build()));
+        WebClient.Builder webClientBuilder = WebClient.builder().exchangeFunction(exchangeFunction);
+        DataFlowRequest dataFlowRequest = dataFlowRequestBuilder().build();
+        GatewayServiceProperties serviceProperties = new GatewayServiceProperties("http://example.com", 2000);
+        ConsentArtefactNotifier dataRequestNotifier = new ConsentArtefactNotifier(webClientBuilder, () -> Mono.just(token),serviceProperties);
+
+        HIUNotificationRequest request = new HIUNotificationRequest(LocalDateTime.now(), UUID.randomUUID(),new ConsentNotifier());
+        StepVerifier.create(dataRequestNotifier.sendConsentArtifactToHIU(request, "1000005"))
+                .expectErrorMatches(throwable -> throwable instanceof ClientError &&
+                        ((ClientError) throwable).getHttpStatus().is5xxServerError())
+                .verify();
+        assertThat(captor.getValue().url().toString()).isEqualTo("http://example.com/consents/hiu/notify");
+        assertThat(captor.getValue().headers().getFirst(AUTHORIZATION)).isEqualTo(token);
+
     }
 }

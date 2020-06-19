b36cb8dfea5fb30df870c300aebeb221d4bad54a
gayatrijagtap <gdjagtap1999@gmail.com>
2020-06-19 16:29:04 +0530

    [G_CM_62] | Neeraj/Gayatri | Remove unnecessary imports

--- src/test/java/in/projecteka/consentmanager/dataflow/DataFlowRequesterTest.java
+++ src/test/java/in/projecteka/consentmanager/dataflow/DataFlowRequesterTest.java
@@ -29,7 +29,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class DataFlowRequesterTest {

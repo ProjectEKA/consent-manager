aa3d93e58a54cc1852eac79cdd1e2a8c2e62b05d
Ganesan Arunachalams <ganesan.arunachalam91@gmail.com>
2020-06-22 12:24:42 +0530

    [G-0] Fix sql setup

--- db-init.sql
+++ db-init.sql
@@ -1,10 +1,5 @@
 create database keycloak;
 create user keycloak with encrypted password 'password';
 grant all privileges on database keycloak to keycloak;
-
-SELECT 'CREATE DATABASE consent_manager'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'consent_manager')\gexec
-SELECT 'CREATE DATABASE otpservice'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'otpservice')\gexec
-SELECT 'CREATE DATABASE health_information_user'
-WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = 'health_information_user')\gexec
+create database consent_manager;
+create database health_information_user;

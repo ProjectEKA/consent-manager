6a5b7a9944b0d192fd6c69aa103e9c8f3a86fbe8
Apoorva-GA <apoorvaga29@gmail.com>
2020-07-07 12:06:41 +0530

    [CM_00] | Remove unused methods from link service client (#396)

--- src/main/java/in/projecteka/consentmanager/clients/LinkServiceClient.java
+++ src/main/java/in/projecteka/consentmanager/clients/LinkServiceClient.java
@@ -1,10 +1,6 @@
 package in.projecteka.consentmanager.clients;
 
-import in.projecteka.consentmanager.clients.model.ErrorRepresentation;
 import in.projecteka.consentmanager.clients.model.PatientLinkReferenceRequest;
-import in.projecteka.consentmanager.clients.model.PatientLinkReferenceResponse;
-import in.projecteka.consentmanager.clients.model.PatientLinkRequest;
-import in.projecteka.consentmanager.clients.model.PatientLinkResponse;
 import in.projecteka.consentmanager.clients.properties.GatewayServiceProperties;
 import in.projecteka.consentmanager.common.ServiceAuthentication;
 import in.projecteka.consentmanager.link.link.model.LinkConfirmationRequest;
@@ -16,7 +12,6 @@ import reactor.core.publisher.Mono;
 import java.time.Duration;
 
 import static in.projecteka.consentmanager.common.Constants.HDR_HIP_ID;
-import static java.util.function.Predicate.not;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 
 @AllArgsConstructor
@@ -26,22 +21,6 @@ public class LinkServiceClient {
     private final WebClient webClientBuilder;
     private final ServiceAuthentication serviceAuthentication;
     private final GatewayServiceProperties gatewayServiceProperties;
-
-    public Mono<PatientLinkReferenceResponse> linkPatientEnquiry(
-            PatientLinkReferenceRequest patientLinkReferenceRequest,
-            String url,
-            String authorization) {
-        return webClientBuilder
-                .post()
-                .uri(String.format("%s/patients/link", url))
-                .header(AUTHORIZATION, authorization)
-                .body(Mono.just(patientLinkReferenceRequest), PatientLinkReferenceRequest.class)
-                .retrieve()
-                .onStatus(not(HttpStatus::is2xxSuccessful), clientResponse ->
-                        clientResponse.bodyToMono(ErrorRepresentation.class)
-                                .flatMap(e -> Mono.error(new ClientError(clientResponse.statusCode(), e))))
-                .bodyToMono(PatientLinkReferenceResponse.class);
-    }
 
     public Mono<Boolean> linkPatientEnquiryRequest(PatientLinkReferenceRequest patientLinkReferenceRequest,
                                                    String authorization,
@@ -63,26 +42,7 @@ public class LinkServiceClient {
                 .thenReturn(Boolean.TRUE);
     }
 
-    public Mono<PatientLinkResponse> linkPatientConfirmation(
-            String linkRefNumber,
-            PatientLinkRequest patientLinkRequest,
-            String url,
-            String authorization) {
-        return webClientBuilder
-                .post()
-                .uri(String.format("%s/patients/link/%s", url, linkRefNumber))
-                .header(AUTHORIZATION, authorization)
-                .body(Mono.just(patientLinkRequest), PatientLinkRequest.class)
-                .retrieve()
-                .onStatus(not(HttpStatus::is2xxSuccessful), clientResponse ->
-                        clientResponse.bodyToMono(ErrorRepresentation.class)
-                                .flatMap(e -> Mono.error(new ClientError(clientResponse.statusCode(), e))))
-                .bodyToMono(PatientLinkResponse.class);
-    }
-
-    public Mono<Boolean> confirmPatientLink(
-            LinkConfirmationRequest confirmationRequest,
-            String hipId) {
+    public Mono<Boolean> confirmPatientLink(LinkConfirmationRequest confirmationRequest, String hipId) {
         return serviceAuthentication.authenticate()
                 .flatMap(authToken ->
                         webClientBuilder

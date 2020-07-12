a748defd3cab6ccc1deeba70fa507f9848625134
Angshuman Sarkar <angshuonline@gmail.com>
2020-07-12 13:39:54 +0530

    downgrading api version

--- src/main/java/in/projecteka/consentmanager/common/Constants.java
+++ src/main/java/in/projecteka/consentmanager/common/Constants.java
@@ -1,7 +1,7 @@
 package in.projecteka.consentmanager.common;
 
 public class Constants {
-    public static final String API_VERSION = "v1";
+    public static final String API_VERSION = "v0.5";
     public static final String BLACKLIST = "blacklist";
     public static final String BLACKLIST_FORMAT = "%s:%s";
     public static final String SCOPE_CONSENT_APPROVE = "consentrequest.approve";

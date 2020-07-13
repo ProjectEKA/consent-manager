ac87a60648fdc729c46a3240ac24c2fbc6f650fa
Aditi Aggarwal <aditiaggarwal@Aditis-MacBook-Pro.local>
2020-07-13 15:48:01 +0530

    [CM_630] | Gayatri/Aditi | Changes error code

--- src/main/java/in/projecteka/consentmanager/clients/model/ErrorCode.java
+++ src/main/java/in/projecteka/consentmanager/clients/model/ErrorCode.java
@@ -52,7 +52,7 @@ public enum ErrorCode {
     REFRESH_TOKEN_INCORRECT(1403),
     INVALID_RESP_REQUEST_ID(1506),
     SERVICE_DOWN(1503),
-    DUPLICATE_DISCOVERY_REQUEST(1409); /*please resume codes from the line above, we will put the codes in order later
+    DISCOVERY_REQUEST_NOT_FOUND(1512); /*please resume codes from the line above, we will put the codes in order later
     and in ranges*/
 
     private final int value;

4dd3e0b441dc7621fb2909e6216815a2b311c05d
gayatrijagtap <44019996+gayatrijagtap@users.noreply.github.com>
2020-07-24 18:55:26 +0530

    remove unnecessary line
    Co-authored-by: Ganesan Arunachalam <ganesan.arunachalam91@gmail.com>

--- src/main/java/in/projecteka/consentmanager/user/UserConfiguration.java
+++ src/main/java/in/projecteka/consentmanager/user/UserConfiguration.java
@@ -198,8 +198,7 @@ public class UserConfiguration {
                 sessionService,
                 otpServiceProperties,
                 dummyHealthAccountService,
-                hasCache
-                );
+                hasCache);
     }
 
     @Bean

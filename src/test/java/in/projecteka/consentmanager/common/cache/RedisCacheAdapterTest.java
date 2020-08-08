85fbf9b508b755eac0bbcc11e8949fb3c4ed43c2
Mritunjay Dubey <mddubey409@gmail.com>
2020-08-08 19:05:05 +0530

    Dubey | 000 | Enable retry for redis operations (#477)

--- src/test/java/in/projecteka/consentmanager/common/cache/RedisCacheAdapterTest.java
+++ src/test/java/in/projecteka/consentmanager/common/cache/RedisCacheAdapterTest.java
@@ -3,8 +3,13 @@ package in.projecteka.consentmanager.common.cache;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveValueOperations;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
 
 import static in.projecteka.consentmanager.common.TestBuilders.aLong;
 import static in.projecteka.consentmanager.common.TestBuilders.string;
@@ -12,13 +17,17 @@ import static java.time.Duration.ofMinutes;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+import static reactor.core.publisher.Mono.defer;
 import static reactor.core.publisher.Mono.empty;
+import static reactor.core.publisher.Mono.error;
 import static reactor.core.publisher.Mono.just;
 import static reactor.test.StepVerifier.create;
 
 class RedisCacheAdapterTest {
 
     public static final int EXPIRATION_IN_MINUTES = 5;
+    private static final int RETRY = 3;
+
     @Mock
     ReactiveRedisOperations<String, String> redisOperations;
 
@@ -27,10 +36,13 @@ class RedisCacheAdapterTest {
 
     private RedisCacheAdapter redisCacheAdapter;
 
+    private RedisCacheAdapter retryableAdapter;
+
     @BeforeEach
     public void init() {
         initMocks(this);
-        redisCacheAdapter = new RedisCacheAdapter(redisOperations, EXPIRATION_IN_MINUTES);
+        redisCacheAdapter = new RedisCacheAdapter(redisOperations, EXPIRATION_IN_MINUTES, 0);
+        retryableAdapter = new RedisCacheAdapter(redisOperations, EXPIRATION_IN_MINUTES, RETRY);
     }
 
     @Test
@@ -93,6 +105,55 @@ class RedisCacheAdapterTest {
 
         create(redisCacheAdapter.increment(key))
                 .assertNext(increment -> assertThat(increment).isEqualTo(expectedIncrement))
+                .verifyComplete();
+    }
+
+
+    @Test
+    void shouldGiveErrorIfErrorWhileFetching() {
+        var key = string();
+        var value = string();
+        when(redisOperations.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.get(key)).thenAnswer(new Answer<Mono<String>>() {
+            private int numberOfTimesCalled = 0;
+
+            @Override
+            public Mono<String> answer(InvocationOnMock invocation) {
+                return defer(() -> {
+                    if (numberOfTimesCalled++ == RETRY) {
+                        return just(value);
+                    }
+                    return error(new Exception("Failed to get"));
+                });
+            }
+        });
+
+        create(redisCacheAdapter.get(key))
+                .expectErrorMessage("Failed to get")
+                .verify();
+    }
+
+    @Test
+    void shouldRetryIfErrorWhileFetchingWhenConfigured() {
+        var key = string();
+        var value = string();
+        when(redisOperations.opsForValue()).thenReturn(valueOperations);
+        when(valueOperations.get(key)).thenAnswer(new Answer<Mono<String>>() {
+            private int numberOfTimesCalled = 0;
+
+            @Override
+            public Mono<String> answer(InvocationOnMock invocation) {
+                return defer(() -> {
+                    if (numberOfTimesCalled++ == RETRY) {
+                        return just(value);
+                    }
+                    return error(new Exception("Connection error"));
+                });
+            }
+        });
+
+        create(retryableAdapter.get(key))
+                .assertNext(actualValue -> assertThat(actualValue).isEqualTo(value))
                 .verifyComplete();
     }
 }

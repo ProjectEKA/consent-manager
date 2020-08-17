1aa50182bd2e082238b6a0c301565b5dbc613aa9
Ganesan Arunachalams <ganesan.arunachalam91@gmail.com>
2020-08-17 09:55:07 +0530

    Use delay timeout exception from library

--- consent/src/main/java/in/projecteka/consentmanager/common/DelayTimeoutException.java
+++
@@ -1,5 +0,0 @@
-package in.projecteka.consentmanager.common;
-
-public final class DelayTimeoutException extends RuntimeException {
-	private static final long serialVersionUID = 1_801_831_526_160_357_413L;
-}

1c1b1e0de25a8060af388e6de47434b4cc1876fb
mounicasagi <mounica6633@gmail.com>
2020-08-25 21:02:16 +0530

    [843_CR_Status] | fixed comments.

--- consent/src/main/java/in/projecteka/consentmanager/clients/ConsentManagerClient.java
+++ consent/src/main/java/in/projecteka/consentmanager/clients/ConsentManagerClient.java
@@ -7,12 +7,15 @@ import in.projecteka.consentmanager.dataflow.model.ConsentArtefactRepresentation
 import in.projecteka.consentmanager.properties.GatewayServiceProperties;
 import in.projecteka.library.clients.model.ClientError;
 import in.projecteka.library.common.ServiceAuthentication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
+import java.util.Properties;
 import java.util.function.Supplier;
 
 import static com.google.common.net.HttpHeaders.AUTHORIZATION;
@@ -21,6 +24,7 @@ import static java.lang.String.format;
 import static java.util.function.Predicate.not;
 
 public class ConsentManagerClient {
+    private static final Logger logger = LoggerFactory.getLogger(ConsentManagerClient.class);
     private static final String CONSENT_REQUEST_INIT_URL_PATH = "/consent-requests/on-init";
     private static final String CONSENT_FETCH_URL_PATH = "/consents/on-fetch";
     private static final String CONSENT_REQUEST_STATUS_URL_PATH = "/consent-requests/on-status";
@@ -108,12 +112,18 @@ public class ConsentManagerClient {
                                 .header(HDR_HIU_ID, hiuId)
                                 .bodyValue(consentStatusResponse)
                                 .retrieve()
-                                .onStatus(httpStatus -> httpStatus.value() == 400,
-                                        clientResponse -> Mono.error(ClientError.unprocessableEntity()))
-                                .onStatus(httpStatus -> httpStatus.value() == 401,
-                                        clientResponse -> Mono.error(ClientError.unAuthorized()))
+                                .onStatus(httpStatus -> httpStatus.value() == HttpStatus.BAD_REQUEST.value(),
+                                        clientResponse -> clientResponse.bodyToMono(Properties.class)
+                                                .doOnNext(properties -> logger.error(properties.toString()))
+                                                .then(Mono.error(ClientError.unprocessableEntity())))
+                                .onStatus(httpStatus -> httpStatus.value() == HttpStatus.UNAUTHORIZED.value(),
+                                        clientResponse -> clientResponse.bodyToMono(Properties.class)
+                                                .doOnNext(properties -> logger.error(properties.toString()))
+                                                .then(Mono.error(ClientError.unAuthorized())))
                                 .onStatus(HttpStatus::is5xxServerError,
-                                        clientResponse -> Mono.error(ClientError.networkServiceCallFailed()))
+                                        clientResponse -> clientResponse.bodyToMono(Properties.class)
+                                                .doOnNext(properties -> logger.error(properties.toString()))
+                                                .then(Mono.error(ClientError.networkServiceCallFailed())))
                                 .toBodilessEntity()
                                 .timeout(Duration.ofMillis(gatewayServiceProperties.getRequestTimeout())))
                 .then();
